c6a88bbf8b1a86f1c621e04525d4ce6765ba70bd
Mohammed Najib Haouas <haouas.najib@hotmail.com>
2017-11-07 18:18:47 -0500

    Travail terminé

--- src/2_43_somme-max-derivee.pl
+++ src/2_43_somme-max-derivee.pl
@@ -19,8 +19,8 @@ d(F,X,G) :- F=A*X^2, atomic(A), T is (A*2), G=T*X, !. % Monôme degré 2, quelco
 d(F,X,G) :- F=X^B, atomic(B), U is (B-1), G=B*X^U, !. % Monôme degré quelconque, coef 1
 d(F,X,G) :- F=A*X^B, atomic(A), atomic(B), T is (A*B), U is (B-1), G=T*X^U, !. % Monôme degré quelconque, coef quelconque
 
-d(F,X,G) :- F=A+B, B\==X, ((B\=X^_,B\=_*X,B\=_*X^_,B\=_+_);atomic(B)), d(A,X,G), !. % Appel récursif pour polynôme, deuxième terme = constante
-d(F,X,G) :- F=A+B, A\==X, ((A\=X^_,A\=_*X,A\=_*X^_,A\=_+_);atomic(A)), d(B,X,G), !. % Appel récursif pour polynôme, premier terme = constante
+d(F,X,G) :- F=A+B, B\==X, ((B\=X^_,B\=_*X,B\=_*X^_,B\=_+_);atomic(B)), d(A,X,G), !. % Appel récursif pour polynôme, deuxième terme = constante ou non reconnu selon la var de dériv
+d(F,X,G) :- F=A+B, A\==X, ((A\=X^_,A\=_*X,A\=_*X^_,A\=_+_);atomic(A)), d(B,X,G), !. % Appel récursif pour polynôme, premier terme = constante ou non reconnu selon la var de dériv
 d(F,X,G) :- F=A+B, d(A,X,U), d(B,X,V), G=U+V, !. % Appel récursif pour polynôme
 
-d(_,_,0). 
+d(_,_,0). % Monome non reconnu.
--- src/4_45_2_crosswords.pl
+++ src/4_45_2_crosswords.pl
@@ -5,5 +5,6 @@ word(anagram, a, n, a, g, r, a, m).
 word(connect, c, o, n, n, e, c, t).
 word(elegant, e, l, e, g, a, n, t).
 
+% Première lettre = position de l'intersection selon la ligne, deuxième lettre selon la colonne.
 crossword(V1,V2,V3,H1,H2,H3) :- word(V1, _, AA, _, BA, _, CA, _), word(V2, _, AB, _, BB, _, CB, _), word(V3, _, AC, _, BC, _, CC, _),
 word(H1, _, AA, _, AB, _, AC, _), word(H2, _, BA, _, BB, _, BC, _), word(H3, _, CA, _, CB, _, CC, _). % une seule solution suffit.
